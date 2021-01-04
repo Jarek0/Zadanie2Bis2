20d9fa8e31565ea1d082ba32763822b72a24b178
Jarek0 <jaroslaw.bielec.1994@gmail.com>
2021-01-04 20:44:22 +0100

    dwdawdaw

--- ConsoleApp1/Equations/QuadraticEquation.cs
+++ ConsoleApp1/Equations/QuadraticEquation.cs
@@ -6,54 +6,106 @@ namespace ConsoleApp1.Equations
     public class QuadraticEquation
     {
 
-        private readonly double _a;
-        private readonly double _b;
-        private readonly double _c;
+        private double _a;
+        public double A
+        {
+            get { return _a; }
+            private set
+            {
+                IParameterValidator aParameterValidator = new NonZeroValidator();
+                if (!aParameterValidator.IsValid(value))
+                {
+                    throw new ArgumentException("Współczynnik a nie może być równy 0. Podaj go ponownie.");
+                }
+                _a = value;
+        } }
+        public double B { get; private set; }
+        public double C { get; private set; }
+        public Delta Delta => new Delta(this);
+
+        public IQuadraticEquationSolution Solution => QuadraticEquationSolver.Solve(this, this.Delta);
 
         private QuadraticEquation(double a, double b, double c)
         {
-            _a = a;
-            _b = b;
-            _c = c;
+            A = a;
+            B = b;
+            C = c;
         }
         
-        public IQuadraticEquationSolution FindSolutionsInRealNumbers(Delta delta)
+        private QuadraticEquation()
         {
-            return QuadraticEquationSolver.Solve(this, delta);
+            _a = 0;
+            B = 0;
+            C = 0;
         }
         
         public override string ToString()
         {
-            return $"({_a}*x^2) + ({_b}*x) + ({_c}) = 0";
+            return $"({A}*x^2) + ({B}*x) + ({C}) = 0";
         }
-
-        public double A => _a;
-
-        public double B => _b;
-        
-        public double C => _c;
-        
         public class QuadraticEquationAssembler
         {
 
             private readonly IEquationParameterProvider _parameterProvider;
-            private readonly IParameterValidator _aParameterValidator = new NonZeroValidator();
+            private const string ParametrizedConstructor = "a";
+            private const string NotParametrizedConstructor = "b";
 
             public QuadraticEquationAssembler(IEquationParameterProvider parameterProvider)
             {
                 this._parameterProvider = parameterProvider;
             }
-            
+
             public QuadraticEquation Assemble()
             {
-                var a = _parameterProvider.Provide("a");
-                if (!_aParameterValidator.IsValid(a))
+                var wayOfCreation = WayOfCreationQuadraticEquation();
+                if (wayOfCreation != null && wayOfCreation.Equals(ParametrizedConstructor))
                 {
-                    throw new ArgumentException("Współczynnik a nie może być równy 0. Podaj go ponownie.");
+                    return AssembleUsingParamConstructor();
                 }
+                if (wayOfCreation != null && wayOfCreation.Equals(NotParametrizedConstructor))
+                {
+                    return AssembleUsingNotParamConstructor();
+                }
+                throw new Exception("Nieprawidłowy wartość określająca sposób tworzenia równania kwadratowego");
+            }
+
+            private static string WayOfCreationQuadraticEquation()
+            {
+                for (;;)
+                {
+                    try
+                    {
+                        Console.WriteLine("Wpisz a żeby stworzyć równanie poprzez konstruktor z parametrami");
+                        Console.WriteLine("Wpisz b żeby stworzyć równanie poprzez konstruktor z parametrami");
+                        string userInput = Console.ReadLine();
+                        if (userInput.Equals(ParametrizedConstructor) || userInput.Equals(NotParametrizedConstructor))
+                        {
+                            return userInput;
+                        }
+                        throw new Exception("Nieprawidłowy wartość określająca sposób tworzenia równania kwadratowego");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Podałeś nieprawidłowy sposób tworzenia równania kwadratowego! Spróbuj jeszcze raz!");
+                    }
+                }
+            }
+            
+            private QuadraticEquation AssembleUsingParamConstructor()
+            {
+                var a = _parameterProvider.Provide("a");
                 var b = _parameterProvider.Provide("b");
                 var c = _parameterProvider.Provide("c");
                 return new QuadraticEquation(a, b, c);
+            }
+            
+            private QuadraticEquation AssembleUsingNotParamConstructor()
+            {
+                var equation = new QuadraticEquation
+                {
+                    A = _parameterProvider.Provide("a"), B = _parameterProvider.Provide("b"), C = _parameterProvider.Provide("c")
+                };
+                return equation;
             }
         }
     }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -19,9 +19,8 @@ namespace ConsoleApp1
                         "Algorytm rozwiazuje równanie postaci a*x^2 + b*x + c = 0 dla zadanego a, b i c w zbiorze liczb rzeczywistych");
                     var equation = equationAssembler.Assemble();
                     Console.WriteLine($"Równanie po podaniu parametrów a, b i c ma postać {equation}");
-                    var delta = new Delta(equation);
-                    Console.WriteLine(delta);
-                    var solutions = equation.FindSolutionsInRealNumbers(delta);
+                    Console.WriteLine(equation.Delta);
+                    var solutions = equation.Solution;
                     solutionPresenter.ShowSolutions(solutions);
                 }
                 catch (Exception e)
